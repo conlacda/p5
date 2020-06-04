6425569f77e6c3a8cb8489597fc88c34114fda68
Pham Hoang Long <phamlong15297@gmail.com>
2020-06-05 01:17:20 +0700

    fix initBoard and draw XO when click

--- tictactoe/sketch.js
+++ tictactoe/sketch.js
@@ -6,12 +6,9 @@ let boundaryY = 5;
 let board = [];
 function initBoard() {
   row = [];
-  for (let i = 0; i < boundaryX; i++) {
-    row.push("");
-  }
-  for (i = 0; i < boundaryY; i++) {
-    board.push(row);
-  }
+  board = Array.from({ length: boundaryX }, () =>
+    Array.from({ length: boundaryY }, () => "")
+  );
 }
 let human = "X";
 let computer = "O";
@@ -31,9 +28,6 @@ function mousePressed() {
 
   if (board[i][j] == "") {
     board[i][j] = currentPlayer;
-    if (currentPlayer == human) {
-      currentPlayer = computer;
-    }
     currentPlayer = currentPlayer == human ? computer : human;
   }
 }
@@ -41,17 +35,30 @@ function mousePressed() {
 function draw() {
   background(255);
   strokeWeight(4); // độ đậm của đường vẽ line()
-
-  for (let i =1;i< boundaryX;i++){ // vẽ tất cả các thanh dọc
-      line(w*i,0,w*i,height)
+  let i;
+  let j;
+  for (i = 1; i < boundaryX; i++) {
+    // vẽ tất cả các thanh dọc
+    line(w * i, 0, w * i, height);
   }
-  for (let j =1;j<boundaryY;j++){ 
-      line(0,h*j,width,h*j)
+  for (j = 1; j < boundaryY; j++) {
+    // vẽ tất cả các thanh ngang
+    line(0, h * j, width, h * j);
   }
-  
-//   line(w, 0, w, height);
-//   line(w * 2, 0, w * 2, height); // x1,y1,x2,y2 -> vẽ 1 đường thằng từ (x1,y1) -> (x2,y2)
-//   line(0, h, width, h);
-//   line(0, h * 2, width, h * 2);
-
+  for (i = 0; i < boundaryX; i++) {
+    for (j = 0; j < boundaryY; j++) {
+      spot = board[i][j];
+      // lấy ra tâm của mỗi ô
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let r = w / 4;
+      if (spot == human) {
+        line(x - r, y - r, x + r, y + r);
+        line(x + r, y - r, x - r, y + r);
+      } else if (spot == computer) {
+        noFill(); // sau lệnh này "các" hình vẽ sẽ trong suốt
+        ellipse(x, y, r * 2);
+      }
+    }
+  }
 }
