76b03af7d6e67d68d9b4837d16f6fcb01bb243c6
Long <phamlong15297@gmail.com>
2020-06-05 16:43:59 +0700

    complete play by turn mode and show result when game ends

--- tictactoe/sketch.js
+++ tictactoe/sketch.js
@@ -4,6 +4,7 @@ let h; // height /3;
 let boundaryX = 5;
 let boundaryY = 5;
 let board = [];
+let connect = 5 // số connect để chiến thắng
 function initBoard() {
   row = [];
   board = Array.from({ length: boundaryX }, () =>
@@ -13,6 +14,8 @@ function initBoard() {
 let human = "X";
 let computer = "O";
 let currentPlayer = human;
+let gameOver = false;
+let winner = "";
 function setup() {
   createCanvas(400, 400); // weight,height: tạo 1 khung hình chữ nhật
   w = width / boundaryX;
@@ -20,15 +23,91 @@ function setup() {
   initBoard();
 }
 
+function value(x, y) {
+  // lấy ra giá trị của bảng và trả về null nếu nằm ngoài bảng
+  if (outOfBoard(x, y)) return "";
+  return board[x][y];
+}
+
+function checkHorizontal(x, y) {
+  // hàm này lấy ra 1 hàng ngang bắt đầu từ điểm (x,y) và kiểm tra xem mảng đó có chỉ 1 loại X,O hay ko
+  let spot_array = [];
+  for (let i = 0; i < connect; i++){
+    spot_array.push(value(x + i, y));
+  }
+  let unique = [... new Set(spot_array)];
+  unique = JSON.stringify(unique);
+  if ( unique === JSON.stringify([human]) || unique === JSON.stringify([computer])) {
+    winner = unique[0]
+    gameOver = true;
+  }
+}
+function checkVertical(x,y) {
+  let spot_array = [];
+  for (let j = 0; j < connect; j++) {
+    spot_array.push(value(x, y+j));
+  }
+  let unique = [... new Set(spot_array)];
+  unique = JSON.stringify(unique);
+  if (unique === JSON.stringify([human]) || unique === JSON.stringify([computer])) {
+    winner = unique[0]
+    gameOver = true;
+  }
+}
+function checkDiagonal(x,y) {
+  // Check đường chéo
+  let spot_array = [];
+  let unique;
+  // Đường chéo sang phải
+  for (let i = 0; i < connect; i++){
+    spot_array.push(value(x + i, y + i))
+  }
+  unique = [... new Set(spot_array)];
+  unique = JSON.stringify(unique);
+  if (unique === JSON.stringify([human]) || unique === JSON.stringify([computer])) {
+    winner = unique[0]
+    gameOver = true;
+  }
+  // Đường chéo sang trái
+  spot_array = []
+  for (let i = 0; i < connect; i++) {
+    spot_array.push(value(x - i, y + i))
+  }
+  unique = [... new Set(spot_array)];
+  unique = JSON.stringify(unique);
+  if (unique === JSON.stringify([human]) || unique === JSON.stringify([computer])) {
+    winner = unique[0]
+    gameOver = true;
+  }
+}
+
+function checkWinner() {
+  // 1 vòng lặp qua tất cả các ô và kiểm tra xem ô đó có phải ô đầu tiên trong chuỗi 3 cái liên tục ko 
+  // Cách khác: kiểm tra xem ô vừa đánh có tạo ra connect-3 để thắng ko (vì những ô khác đã kiểm tra vào lần trước và lần kiểm tra sau kiểm tra lại gây thừa thao tác)
+  // Horizontal
+  for (let i = 0; i < boundaryX; i++){
+    for (let j = 0; j < boundaryY; j++){
+      checkHorizontal(i, j);
+      checkVertical(i, j);
+      checkDiagonal(i, j);
+    }
+  }
+}
+
+function outOfBoard(x, y) {
+  return (0 > x || x >= boundaryX || y >= boundaryY || y < 0);   
+}
 function mousePressed() {
   // mouseX, mouseY : tọa độ khi ấn chuột
 
   let i = floor(mouseX / w);
   let j = floor(mouseY / h);
-
-  if (board[i][j] == "") {
-    board[i][j] = currentPlayer;
-    currentPlayer = currentPlayer == human ? computer : human;
+  if (!outOfBoard(i, j)) {
+    if (board[i][j] == "") {
+      board[i][j] = currentPlayer;
+      currentPlayer = currentPlayer == human ? computer : human;
+    }
+    checkWinner();
   }
 }
 
@@ -60,5 +139,9 @@ function draw() {
         ellipse(x, y, r * 2);
       }
     }
+  }
+  if (gameOver) {
+    noLoop(); // dừng hàm draw() 
+    alert(winner + ' wins');
   }
 }
